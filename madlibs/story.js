d0165e537cf56092bf5116f403b01834628e8374
Lindsay Williams <williamsl25@yahoo.com>
2015-09-24 18:03:40 -0400

    fixed js file

--- madlibs/story.js
+++ madlibs/story.js
@@ -15,10 +15,11 @@ var nounTwo = prompt("please pick a noun");
 var adverb = prompt("Pick an adverb for example, slowly or quietly");
 var verbThree = prompt("Pick a verb");
 var verbFour = prompt("Pick a verb");
-// var relativeTwo = prompt("Please type a relative's name");
+
 var personTwo = prompt("Please name someone in the room");
 
 alert("All done. Ready for your story?");
 
 
-document.write("Dear "+ relativeOne + ", I am having a(n) " + adjectiveOne  +" time at camp. The counselor is " + adjectiveTwo + " and the food is " + adjectiveThree + "!" + " I met  " + personOne + " and we became " + adjectiveFour + " friends." + " Unfortunately I " + verbOne + " my " + bodyPart + " so we couldn't go " + verbTwo + " like everbody else. I need more " + nounOne + " and a " + nounTwo + " sharpener, so please " + adverb + " " verbThree + " more when you " + verbFour + ". I miss you, Love " + personTwo );
+document.write("Dear "+ relativeOne + ", I am having a " + adjectiveOne+" time at camp. The counselor is " + adjectiveTwo + " and the food is " + adjectiveThree + "!" + " I met  " + personOne + " and we became " + adjectiveFour + " friends." + " Unfortunately I " + verbOne + " my " + bodyPart + " so we couldn't go " + verbTwo + " like everbody else. I need more " + nounOne + " and a " + nounTwo + " sharpener, so please " + adverb + " " + verbThree + " more when you " + verbFour + ". I miss you, Love " + personTwo);
+ 
